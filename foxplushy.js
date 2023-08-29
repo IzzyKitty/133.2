c3b558b62fbcb121a93df45f4dc34fc5cd53dd78
IzzyKitty <115121463+IzzyKitty@users.noreply.github.com>
2023-08-29 18:58:16 -0500

    Add files via upload

---
+++ foxplushy.js
@@ -0,0 +1,39 @@
+image1=""
+
+function setup(){
+canvas=createCanvas(640,420)
+canvas.center()
+}
+
+function preload(){
+image1=loadImage("water_bottle.jpg")
+}
+
+function modelloaded(){
+console.log("model has been loaded");
+objectDetector.detect(image1,gotresults)
+}
+
+function gotresults(error,results){
+if(error){
+console.error(error);
+}
+console.log(results);
+objects=results
+}
+
+function draw(){
+image(image1,0,0,640,420)
+
+if(status1!=""){
+for(i=0; i<objects.length; i++){
+document.getElementById("status").innerHTML="status:object detected"
+fill("#8000ff")
+percent=floor(objects[0].confidence*100)
+text(objects[0].label+""+percent+"%",objects[0].x,objects[0].y)
+noFill()
+stroke("#8000ff")
+rect(objects[0].x,objects[0].y,objects[0].hight,objects[0].width)
+}
+}
+}
